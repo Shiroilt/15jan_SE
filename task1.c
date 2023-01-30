d1b2492f0ff93b2a589dfc0bc0d393a3f06450b6
Shiroilt <work102304@gmail.com>
2023-01-30 08:48:46 +0530

    modul 1

---
+++ task1.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+	printf("name:Shashwat\nage:19\nbod:23-1-2004\naddress:Ahemdabad");
+}
---
+++ task2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a;
+	int b,c;
+	scanf("%d",&a);
+	b=a*a*a;
+	c=a*a;
+	printf("square ::%d\ncube ::%d",c,b);
+}
